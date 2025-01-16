17d7a0e04410a39e90cf80ce73f089c4f06a9a25
Lucas Vandermaarel <Lukevdm64@gmail.com>
2025-01-15 20:35:37 -0500

    vault backup: 2025-01-15 20:35:37

--- Centennial/Term 2/COMP125/Assignment1/LucasVandermaarel_Assignment1.js
+++ Centennial/Term 2/COMP125/Assignment1/LucasVandermaarel_Assignment1.js
@@ -2,27 +2,33 @@
 
 
 const createTable = (rows, cols) => {
-    let table = document.getElementById("dynamic-table");
+    const table = document.getElementById("dynamic-table");
+
+    const tableElement = document.createElement("table");
     // reset table
-    table.innerHTML = "";
+    tableElement.innerHTML = "";
 
     for (let i = 0; i < rows; i++) {
-        let row = table.insertRow(i);
+        let row = document.createElement("tr");
         for (let j = 0; j < cols; j++) {
-            let cell = row.insertCell(j);
-            cell.innerHTML = `${i + 1}, ${j + 1}`;
+            let cell = document.createElement("td");
+            cell.innerHTML = `Row No: ${i + 1} | Col No:${j + 1}`;
+            row.append(cell);
         }
+        tableElement.append(row);
     }
+    table.append(tableElement);
 }
 
 function init() {
     // initial table with 4 rows and 5 columns
-    createTable(4,5);
+    createTable(4, 5);
 }
 
-document.querySelector('button').onclick = () => {
+document.getElementById('submitBtn').onclick = () => {
     let rows = document.getElementById('rows').value;
     let cols = document.getElementById('cols').value;
-    createTable(rows, cols)};
+    createTable(rows, cols)
+};
 window.addEventListener("load", init);
 
