7664c162fb8977ac3fddb56c071e6db7b2f47c85
Lucas Vandermaarel <Lukevdm64@gmail.com>
2025-01-30 11:29:03 -0500

    vault backup: 2025-01-30 11:29:03

--- Centennial/Term 2/COMP125/Assignment 2/functions.js
+++ Centennial/Term 2/COMP125/Assignment 2/functions.js
@@ -3,7 +3,14 @@ function showPreviousCourses() {
         // Select the unordered list in the courses section
         const coursesSection = document.querySelector('#courses > ul');
         // Initialize and Assign the courses to an array
-        let courseStrings = ["MATH175 - Functions & Number Systems", "GNED137 - A Wellness Approach", "COMP213 - Web Interface Design", "COMP120 - Software Engineering Fundamentals", "COMP100 - Programming 1", "COMM170 - College Communications 2"];
+        let courseStrings = [
+            "MATH175 - Functions & Number Systems", 
+            "GNED137 - A Wellness Approach", 
+            "COMP213 - Web Interface Design", 
+            "COMP120 - Software Engineering Fundamentals", 
+            "COMP100 - Programming 1", 
+            "COMM170 - College Communications 2"
+        ];
 
         // Add each course to the list
         courseStrings.forEach((course) => {
@@ -41,7 +48,22 @@ function addReactSkill() {
 window.onload = function () {
     let userName = prompt("Please enter your name:");
     if (userName) {
-        alert("Welcome, " + userName + "!");
+        const welcomeSection = document.getElementById('welcome');
+        let welcomeH1 = document.createElement('h1');
+        welcomeH1.style.textAlign = 'center';
+        welcomeH1.style.margin = 'auto';
+        welcomeH1.style.border = '2px solid #74a0d3';
+        welcomeH1.style.borderRadius = '10px';
+        welcomeH1.style.width = '500px';
+        welcomeH1.style.padding = '5px';
+
+        let welcomeName = document.createElement('span');
+        welcomeName.style.color = 'green';
+        welcomeName.innerHTML = userName + "!";
+
+        welcomeH1.innerHTML = `Welcome, `;
+        welcomeH1.appendChild(welcomeName);
+        welcomeSection.appendChild(welcomeH1);
     } else {
         alert("You did not enter a name!");
     }
