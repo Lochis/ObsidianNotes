f428e5fdb469a073087cf5b404d343c0d1379fb5
Lucas Vandermaarel <Lukevdm64@gmail.com>
2025-02-05 21:07:41 -0500

    vault backup: 2025-02-05 21:07:41

--- Centennial/Term 2/COMP122/Assignment1_PartA_Q1.sql
+++ Centennial/Term 2/COMP122/Assignment1_PartA_Q1.sql
@@ -3,7 +3,7 @@ CREATE TABLE STUDENT
 Lastname VARCHAR2(50) NOT NULL,
 Firstname VARCHAR2(50) NOT NULL,
 ProgramCode Number(4),
-Email VARCHAR(29)
+Email VARCHAR(30)
 -- Wild card, @, wild card, . for ending, and wild card
 CONSTRAINT chk_email CHECK (Email LIKE '%@%.%') 
 );
@@ -11,6 +11,7 @@ CONSTRAINT chk_email CHECK (Email LIKE '%@%.%')
 INSERT INTO STUDENT(StudentId, Lastname, Firstname, ProgramCode, Email)
 VALUES(300111222, 'Anna', 'Rav', 3409, 'ranna@my.centennialcollege.ca');
 
-INSERT INTO STUDENT (StudentId, Lastname, Firstname, ProgramCode, Email)
-VALUES(300123123, 'Chong', 'Raul', 3409, 'rchong@my.centennialcollege.ca'),
-(300124124, 'Vasudevan', 'Hima', 3408, 'hvasu@my.centennialcollege.ca');
+INSERT ALL 
+    INTO STUDENT (StudentId, Lastname, Firstname, ProgramCode, Email) VALUES (300123123, 'Chong', 'Raul', 3409, 'rchong@my.centennialcollege.ca')
+    INTO STUDENT (StudentId, Lastname, Firstname, ProgramCode, Email) VALUES (300124124, 'Vasudevan', 'Hima', 3408, 'hvasu@my.centennialcollege.ca')
+SELECT 1 FROM DUAL;
