02b36c46156e35e160550fe9b83c16ae11bf1b8e
Lucas Vandermaarel <Lukevdm64@gmail.com>
2025-02-05 20:07:40 -0500

    vault backup: 2025-02-05 20:07:40

--- Centennial/Term 2/COMP122/Assignment1_PartA_Q1.sql
+++ Centennial/Term 2/COMP122/Assignment1_PartA_Q1.sql
@@ -1,8 +1,16 @@
-CREATE TABLE Student
+CREATE TABLE STUDENT
 (StudentId NUMBER(9) PRIMARY KEY,
 Lastname VARCHAR2(50) NOT NULL,
 Firstname VARCHAR2(50) NOT NULL,
 ProgramCode Number(4),
 Email VARCHAR(29)
-CONSTRAINT chk_email CHECK (Email LIKE '%@%.%') -- Wild card, @, wild card, . for ending, and wild card
+-- Wild card, @, wild card, . for ending, and wild card
+CONSTRAINT chk_email CHECK (Email LIKE '%@%.%') 
 );
+
+INSERT INTO STUDENT(StudentId, Lastname, Firstname, ProgramCode, Email)
+VALUES(300111222, 'Anna', 'Rav', 3409, 'ranna@my.centennialcollege.ca');
+
+INSERT INTO STUDENT (StudentId, Lastname, Firstname, ProgramCode, Email)
+VALUES(300123123, 'Chong', 'Raul', 3409, 'rchong@my.centennialcollege.ca'),
+(300124124, 'Vasudevan', 'Hima', 3408, 'hvasu@my.centennialcollege.ca');
