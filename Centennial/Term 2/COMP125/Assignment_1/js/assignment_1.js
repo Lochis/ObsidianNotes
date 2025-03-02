ab14a7b4d39c0cc69036393d6dacebe091432a60
Lucas Vandermaarel <Lukevdm64@gmail.com>
2025-03-01 20:49:13 -0500

    vault backup: 2025-03-01 20:49:13

---
+++ Centennial/Term 2/COMP125/Assignment_1/js/assignment_1.js
@@ -0,0 +1,12 @@
+console.log("hello");
+
+// make a table of the following values:
+// 1. 1
+// 2. 2
+// 3. 3
+// 4. 4
+// 5. 5
+
+// Create a table of the specified values
+const values = [1, 2, 3, 4, 5];
+console.table(values);
