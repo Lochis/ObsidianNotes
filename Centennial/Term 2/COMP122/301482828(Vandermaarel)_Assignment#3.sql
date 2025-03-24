929d466590e83dc57a717f077646ca61cd0e9e0c
Lucas Vandermaarel <Lukevdm64@gmail.com>
2025-03-24 14:08:44 -0400

    vault backup: 2025-03-24 14:08:44

--- Centennial/Term 2/COMP122/301482828(Vandermaarel)_Assignment#3.sql
+++ Centennial/Term 2/COMP122/301482828(Vandermaarel)_Assignment#3.sql
@@ -10,7 +10,9 @@ LEFT JOIN
     db4bikestore_sales_customers c 
 ON 
     o.CUSTOMER_ID = c.CUSTOMER_ID
-WHERE o.CUSTOMER_ID >=327;
+WHERE 
+    o.CUSTOMER_ID >=327
+    AND ROWNUM <= 7;
 
 
 -- 2
@@ -51,5 +53,50 @@ LEFT JOIN
     db4bikestore_sales_customers c
 ON 
     o.CUSTOMER_ID = c.CUSTOMER_ID
-WHERE o.ORDER_ID = 1348;
+WHERE 
+    o.ORDER_ID >= 1348
+    AND (s.FIRST_NAME = 'Mireya' AND s.LAST_NAME = 'Copeland')
+ORDER BY o.ORDER_ID;
     
+-- 4
+SELECT
+    DISTINCT s.FIRST_NAME || s.LAST_NAME AS Staff_Name
+FROM
+    DB4BIKESTORE_SALES_STAFFS s
+LEFT JOIN
+    DB4BIKESTORE_SALES_ORDERS o
+ON
+    o.STAFF_ID = s.STAFF_ID
+LEFT JOIN
+    DB4BIKESTORE_SALES_ORDER_ITEMS i 
+ON
+    o.ORDER_ID = i.ORDER_ID
+WHERE
+    i.DISCOUNT > 0.05;
+
+
+-- 5
+SELECT * FROM USER_SYS_PRIVS;
+
+-- 6 
+SELECT
+    DISTINCT c.FIRST_NAME || c.LAST_NAME AS Customer_Name
+FROM
+    db4bikestore_sales_orders o
+LEFT JOIN
+    db4bikestore_sales_customers c
+ON
+    o.CUSTOMER_ID = c.CUSTOMER_ID
+WHERE
+    o.STORE_ID = 2;
+
+-- 7
+GRANT ALL PRIVILEGES TO PUBLIC;
+
+-- 8 
+CREATE ROLE new_role;
+GRANT CREATE SESSION TO new_role;
+GRANT CREATE TABLE TO new_role;
+GRANT CREATE VIEW TO new_role;
+GRANT SELECT ON db4bikestore_brands TO new_role;
+GRANT INSERT ON db4bikestore_brands TO new_role;
